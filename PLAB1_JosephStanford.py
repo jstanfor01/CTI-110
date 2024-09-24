8be999ac901cf53d69da9a3bb671b1fb2036af83
jstanfor01 <stanforj9242@student.faytechcc.edu>
2024-09-24 16:04:42 -0400

    Add files via upload

---
+++ PLAB1_JosephStanford.py
@@ -0,0 +1,9 @@
+# Joseph Stanford
+# 9/24/2024
+# PLAB1
+# Input and Output
+
+name = input("Enter your first name: ")
+name2 = input("Enter your las name: ")
+
+print(f"Hello, {name} {name2}! Welcome to CTI-110")
