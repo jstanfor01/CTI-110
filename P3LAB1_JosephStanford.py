2c8f4e748a4a56c9396fa8eab91d1ee43f290f1e
jstanfor01 <stanforj9242@student.faytechcc.edu>
2024-10-14 11:43:07 -0400

    Add files via upload

---
+++ P3LAB1_JosephStanford.py
@@ -0,0 +1,19 @@
+# Joseph Stanford
+# 10/14/2024
+# P3LAB1
+# Calculate the most efficient coin combination
+
+# Regular division
+
+print(100/3)
+
+
+# Floor division - returns the integer portion of
+
+print(100//3)
+
+
+# Modulo - returns ONLY the remainder
+
+print(100%3)
+print(7%4)
